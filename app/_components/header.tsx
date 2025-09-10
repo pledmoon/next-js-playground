4ffee54af1054955f330dcea05619e38bd8e761d
Pavel Damaskin <falard.damaskin@gmail.com>
2025-09-10 17:02:33 +0300

    feat: add starter files, add layout and core structure

---
+++ app/_components/header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from '@/app/_components/logo'
+import { Nav } from '@/app/_components/nav'
+
+export const Header = () => {
+  return (
+    <header>
+      <Logo />
+
+      <Nav />
+    </header>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Header } from '@/app/_components/header'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <Header />
+
+        <main>{children}</main>
+
+        <footer>&copy; Copyright</footer>
+      </body>
     </html>
   )
 }
