39a232c04d3c710e9b8669a996943d5081fa4c43
Pavel Damaskin <falard.damaskin@gmail.com>
2025-09-15 16:32:59 +0300

    feat: add simple fetch request and loading.tsx

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,18 @@
-export default function CabinsPage() {
+export default async function CabinsPage() {
+  const url = 'https://jsonplaceholder.typicode.com/users'
+
+  const response = await fetch(url)
+  const data = await response.json()
+
   return (
     <div>
       <h1>Cabins Page</h1>
+      <br />
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
