b506d56338f009514b2c501bdbf983dcac209645
Pavel Damaskin <falard.damaskin@gmail.com>
2026-02-07 17:10:14 +0200

    feat: add client component TextExpander

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { getCabin, getCabins } from '@/app/_lib/data-service'
 import { type Metadata } from 'next'
 import { notFound } from 'next/navigation'
+import TextExpander from '@/app/_components/text-expander'
 
 interface CabinDetailPageProps {
   params: Promise<{ cabinId: string }>
@@ -60,7 +61,11 @@ export default async function CabinDetailPage({ params }: CabinDetailPageProps) 
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          {description && (
+            <p className="text-lg text-primary-300 mb-10">
+              <TextExpander>{description}</TextExpander>
+            </p>
+          )}
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
