237d224ed20416e9fc4f785845cada213658fcea
Pavel Damaskin <falard.damaskin@gmail.com>
2026-04-21 17:00:11 +0300

    feat (redux): add actions

--- app/_store/store.ts
+++ app/_store/store.ts
@@ -33,6 +33,7 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer)
 
+/*
 store.dispatch({ type: 'account/deposit', payload: 100 })
 
 console.log(store.getState())
@@ -48,5 +49,44 @@ store.dispatch({
 console.log(store.getState())
 
 store.dispatch({ type: 'account/payLoan' })
+
+console.log(store.getState())
+*/
+
+/**
+ * Actions creators - это соглашение, работать можно и без них,
+ * это функции, возвращающие actions
+ * для каждого action, создаем свой action creator
+ * возвращает action, а action это объект
+ */
+function deposit(amount: number) {
+  return { type: 'account/deposit', payload: amount }
+}
+
+function withdraw(amount: number) {
+  return { type: 'account/withdraw', payload: amount }
+}
+
+function requestLoan(amount: number, purpose: string) {
+  return {
+    type: 'account/requestLoan',
+    payload: {
+      amount,
+      purpose,
+    },
+  }
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' }
+}
+
+store.dispatch(deposit(500))
+store.dispatch(withdraw(200))
+store.dispatch(requestLoan(1000, 'Buy a cheap car'))
+
+console.log(store.getState())
+
+store.dispatch(payLoan())
 
 console.log(store.getState())
