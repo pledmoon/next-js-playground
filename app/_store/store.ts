ba50f8aa0664c4dc42735f74c5f95a45b2016da8
Pavel Damaskin <falard.damaskin@gmail.com>
2026-04-22 11:55:22 +0300

    feat (redux): add ts

--- app/_store/store.ts
+++ app/_store/store.ts
@@ -1,12 +1,87 @@
 import { createStore } from 'redux'
 
+/**
+ * Base state
+ */
+type State = {
+  balance: number
+  loan: number
+  loanPurpose: string
+}
+
+/**
+ * Set of actions
+ */
+type DepositAction = {
+  type: 'account/deposit'
+  payload: number
+}
+
+type WithdrawAction = {
+  type: 'account/withdraw'
+  payload: number
+}
+
+type RequestLoanAction = {
+  type: 'account/requestLoan'
+  payload: {
+    amount: number
+    purpose: string
+  }
+}
+
+type PayLoanAction = {
+  type: 'account/payLoan'
+}
+
+type Action = DepositAction | WithdrawAction | RequestLoanAction | PayLoanAction
+
+/**
+ * Advanced
+ */
+/*const actions = {
+  deposit: (amount: number) => ({
+    type: 'account/deposit',
+    payload: amount,
+  }),
+  withdraw: (amount: number) => ({
+    type: 'account/withdraw',
+    payload: amount,
+  }),
+} as const
+
+type Action = ReturnType<(typeof actions)[keyof typeof actions]>*/
+
+/**
+ * Synchronously sends an action to the store's reducer, along with the previous state
+ * returned by the store, to mutate the state. Returns the action it was passed.
+ */
+type BaseDispatch = (a: Action) => Action
+
+/**
+ * A store is an object that holds the application's state tree. There should only be a
+ * single store in a Redux app. Instead of multiple stores, you can compose reducers.
+ */
+type Store = {
+  // Dispatches an action. This is the only way to trigger a state change.
+  dispatch: BaseDispatch
+
+  // Return the current state.
+  getState: () => State
+}
+
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 }
 
-const reducer = (state = initialState, action) => {
+/**
+ * Reducers must be pure functions, without API calls
+ * For an unrecognized action, it should return the state unmodified (to be compatible with `combineReducers`)
+ * If the given state is undefined, it should return an initial state that is not undefined.
+ */
+const reducer = (state: State = initialState, action: Action) => {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload }
@@ -31,7 +106,7 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer)
+const store: Store = createStore(reducer)
 
 /*
 store.dispatch({ type: 'account/deposit', payload: 100 })
@@ -59,11 +134,11 @@ console.log(store.getState())
  * для каждого action, создаем свой action creator
  * возвращает action, а action это объект
  */
-function deposit(amount: number) {
+function deposit(amount: number): DepositAction {
   return { type: 'account/deposit', payload: amount }
 }
 
-function withdraw(amount: number) {
+function withdraw(amount: number): WithdrawAction {
   return { type: 'account/withdraw', payload: amount }
 }
 
@@ -74,11 +149,11 @@ function requestLoan(amount: number, purpose: string) {
       amount,
       purpose,
     },
-  }
+  } as const
 }
 
 function payLoan() {
-  return { type: 'account/payLoan' }
+  return { type: 'account/payLoan' } as const
 }
 
 store.dispatch(deposit(500))
